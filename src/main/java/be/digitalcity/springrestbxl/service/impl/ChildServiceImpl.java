0d336f0c9e3eae872e1bdefb7b87384267a4febf
thabitor <thabithamror@gmail.com>
2022-08-16 14:14:18 +0200

    Added Swagger

--- src/main/java/be/digitalcity/springrestbxl/service/impl/ChildServiceImpl.java
+++ src/main/java/be/digitalcity/springrestbxl/service/impl/ChildServiceImpl.java
@@ -6,35 +6,38 @@ import be.digitalcity.springrestbxl.exceptions.InvalidReferenceException;
 import be.digitalcity.springrestbxl.mapper.ChildMapper;
 import be.digitalcity.springrestbxl.model.dto.ChildDTO;
 import be.digitalcity.springrestbxl.model.entities.Child;
+import be.digitalcity.springrestbxl.model.entities.Reservation;
 import be.digitalcity.springrestbxl.model.entities.Tutor;
 import be.digitalcity.springrestbxl.model.forms.ChildInsertForm;
 import be.digitalcity.springrestbxl.model.forms.ChildUpdateForm;
 import be.digitalcity.springrestbxl.repository.ChildRepository;
+import be.digitalcity.springrestbxl.repository.ReservationRepository;
 import be.digitalcity.springrestbxl.repository.TutorRepository;
 import be.digitalcity.springrestbxl.service.ChildService;
-import be.digitalcity.springrestbxl.service.TutorService;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Service
 public class ChildServiceImpl implements ChildService {
 
     private final ChildRepository repository;
     private final TutorRepository tutorRepository;
+    private final ReservationRepository reservationRepository;
     private final ChildMapper mapper;
 
-    public ChildServiceImpl(ChildRepository repository, ChildMapper mapper, TutorRepository tutorRepository) {
+    public ChildServiceImpl(ChildRepository repository, ChildMapper mapper, TutorRepository tutorRepository, ReservationRepository reservationRepository) {
         this.repository = repository;
         this.mapper = mapper;
         this.tutorRepository = tutorRepository;
+        this.reservationRepository = reservationRepository;
     }
 
 
@@ -129,6 +132,18 @@ public class ChildServiceImpl implements ChildService {
     public List<ChildDTO> getAllWithAllergy(String allergy) {
         return repository.findByAllergiesContaining(allergy).stream()
                 .map(mapper::toChildDto)
+                .toList();
+    }
+
+    @Override
+    public List<ChildDTO> getAllPresentOnDay(LocalDate date) {
+        LocalDateTime startOfDay = date.atStartOfDay();
+        LocalDateTime endOfDay = date.atTime(23,59,59);
+
+        return reservationRepository.findByArriveAfterAndDepartBeforeAndCancelFalse(startOfDay, endOfDay).stream()
+                .map(Reservation::getChild )
+                .map( mapper::toChildDto )
+                .distinct()
                 .toList();
     }
 }
